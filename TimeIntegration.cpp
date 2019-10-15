21f088049b31018265f7b5ed261dbb285dfe5e8b
Pooja Rao <poojarao12@gmail.com>
2019-10-15 15:55:58 -0400

    Added time int cpp file

---
+++ TimeIntegration.cpp
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "TimeIntegration.h"
+
+TimeIntegration::TimeIntegration()
+{
+  cout << "Time integration object is being created" << endl;
+}
+
+TimeIntegration::~TimeIntegration(void)
+{
+  cout << "Time integration object is being deleted" << endl;
+}
+
+
+
---
+++ TimeIntegration.h
@@ -0,0 +1,14 @@
+#ifndef TimeIntegration_h
+#define TimeIntegration_h
+
+class TimeIntegration
+{
+  public:
+    TimeIntegration(); // constructor
+    ~TimeIntegration(); // destructor
+  private:
+      
+
+};
+
+#endif
